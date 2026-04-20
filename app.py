0d4b6a4a5431bc702532f49f8daf9d207f287ffc
Mohamed Elgaki <124015654+ASTROBOX1@users.noreply.github.com>
2026-04-20 11:16:27 +0200

    refactor base router to include environment variable for app name

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
 from routes.base import router as base_router
+from dotenv import load_dotenv
+import os
+
+load_dotenv(".env")
 app = FastAPI()
 app.include_router(base_router)
--- routes/base.py
+++ routes/base.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI,APIRouter
-router = APIRouter()
+from dotenv import load_dotenv
+import os
+load_dotenv(".env")
+
+router = APIRouter(
+    prefix="/base",
+    tags=["base"],
+    responses={404: {"description": "Not found"}},
+
+)
 @router.get("/")
-def read_root():
-    return {"Hello": "all"}
+
+async def read_root():
+    app_name = os.getenv("APP_NAME", "Unknown App")
+    return {"Hello": "all", "app_name": app_name}
 
